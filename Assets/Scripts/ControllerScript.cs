777995dd0592aa9b936292e8431722c511ac3eb0
Daniel Max <danielmaxsiegel@gmail.com>
2018-09-17 18:19:52 -0700

    initial commit

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class ControllerScript : MonoBehaviour {
+    AudioSource aSource;
+    public float[] samples = new float[512];
+
+	// Use this for initialization
+	void Start () {
+        aSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+	}
+
+    void GetSpectrumAudioSource() {
+        aSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
