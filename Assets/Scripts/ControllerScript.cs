556e6327657be06bc17779a28c1d701e3baa7d3c
Daniel Max <danielmaxsiegel@gmail.com>
2018-09-19 18:45:55 -0700

    V1 assets

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ControllerScript : MonoBehaviour {
     AudioSource aSource;
     public static float[] samples = new float[512];
+    public static float[] frequencies = new float[8];
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +16,32 @@ public class ControllerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         GetSpectrumAudioSource();
+        makeFrequencyBands();
 	}
 
     void GetSpectrumAudioSource() {
         aSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    // divide frequencies into bands
+    void makeFrequencyBands(){
+        int count = 0;
+
+        for (int i = 0; i < 8; ++i){
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if(i == 7){
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; ++j){
+                average += samples[count] * count + 1;
+                    count++;
+            }
+            average /= count;
+
+            frequencies[i] = average * 10;
+        }
     }
 }
--- Assets/Scripts/Instantiate1.cs
+++ Assets/Scripts/Instantiate1.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class Instantiate1 : MonoBehaviour {
     public GameObject samplePrefab;
     GameObject[] samplesArr = new GameObject[512];
+    public float maxScale = 1000;
 
 	// Use this for initialization
 	void Start () {
-		
+        for (int i = 0; i < 512; ++i){
+            GameObject instanceOfSample = (GameObject)Instantiate(samplePrefab);
+            instanceOfSample.transform.position = this.transform.position;
+            instanceOfSample.transform.parent = this.transform;
+            instanceOfSample.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            instanceOfSample.transform.position = Vector3.forward * 100;
+            samplesArr[i] = instanceOfSample;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        this.transform.Rotate(this.transform.position, 90f);
+            
+        for (int i = 0; i < 512; ++i){
+            if(samplesArr != null){
+                samplesArr[i].transform.localScale = new Vector3(10, (ControllerScript.samples[i] * maxScale) + 2, 10);
+            }
+        }
 	}
 }
